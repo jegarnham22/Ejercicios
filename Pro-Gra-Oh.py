49078375b4cbfeedded77b2adb04e5c4cbd64fd4
Juan Esteban <jegarnham@uc.cl>
2022-03-15 09:32:53 -0300

    funcion atacar lista

--- Pro-Gra-Oh.py
+++ Pro-Gra-Oh.py
@@ -1,5 +1,5 @@
 import os
-import random as rm
+import random
 class Juego:
     
     def __init__(self, turnos):
@@ -16,46 +16,49 @@ class Juego:
         rutaarchivo=os.path.join("cartas.csv")
         with open(rutaarchivo, "rt") as archivo:
             lineas=archivo.readlines()
-        mazo=[]
+
         for m in lineas:
             monstruo=m.strip().split(",")
-            mazo.append(monstruo)
-        mazo.remove(['nombre', 'ataque', 'defensa'])
-        print(mazo)
+            self.mazo.append(monstruo)
+        self.mazo.remove(['nombre', 'ataque', 'defensa'])
+       
     
     def repartir_cartas(self):
-        j1=[]
-        j2=[]
         rangot=10
         rangom=5
-        random.shuffle(mazo)
+        random.shuffle(self.mazo)
         for i in range(0,rangom):
-            j1.append(mazo[i])
+            self.j1.append(self.mazo[i])
         for k in range(rangom,rangot):
-            j2.append(mazo[k])
+            self.j2.append(self.mazo[k])
     
     def atacar(self, atacante, defensor):
-        random.shuffle(j1)
-        random.shuffle(j2)
-        ataque=atacante[0][1]
-        defensa=defensa[0][2]
-        ptos_ataque = atacante.ataque
-        ptos_defensa = defensor.defensa
-        # if ptos_ataque > ptos_defensa:
+        random.shuffle(atacante)
+        random.shuffle(defensor)
+        ptos_ataque=atacante[0][1]
+        ptos_defensa=defensor[0][2]
+        if ptos_ataque > ptos_defensa:
+            defensor.pop(0)
+            print(f"el jugador atacante gana este turno")
+        elif ptos_defensa > ptos_ataque:
+            atacante.pop(0)
+            print(f"el jugador defensor gana este turno")
 
         # # Rellenar aquí
     
     def comenzar_juego(self, turnos):
         for i in range(1, turnos + 1):
             print(f"Turno número {i}")
-            if i % 2:
+            if i % 2 and len(self.j1)>0 and len(self.j2)>0:
+                self.atacar(self.j1,self.j2)
                 # Ataca el jugador 1
                 # Rellenar aquí
-                pass
-            else:
+                
+            elif len(self.j1)>0 and len(self.j2)>0:
+                self.atacar(self.j2,self.j1)
                 # Ataca el jugador 2
                 # Rellenar aquí
-                pass
+                
 
 
 juego = Juego(10)
