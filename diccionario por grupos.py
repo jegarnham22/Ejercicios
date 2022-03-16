56bafbc392575640d8564f00b52c6e1c4df3c138
Juan Esteban <jegarnham@uc.cl>
2022-03-16 14:14:16 -0300

    diccionario por grupos listo

---
+++ diccionario por grupos.py
@@ -0,0 +1,40 @@
+from random import choice, randint
+from collections import namedtuple, defaultdict # Puedes usar defaultdict si lo deseas
+
+def dic1(paises,personas):
+    personas_por_paises={}
+    for pais in paises:
+        personaspais=[]
+        for pp in range(0,len(personas)):
+            if pais==personas[pp][1]:
+                personaspais.append(personas[pp])
+        personas_por_paises[pais]=personaspais
+
+    #print(personas_por_paises)
+
+paises = ['Argentina', 'Bolivia', 'Chile', 'Perú','Colombia']
+Persona = namedtuple("Persona", 'edad, nacionalidad')
+
+personas = []
+for i in range(100):
+    nueva_persona = Persona(randint(0, 100), choice(paises))
+    personas.append(nueva_persona)
+
+i = 0
+personas_por_edad={}
+while i < 100:
+    edadpersonas=[]
+    tuplaedad=(i,i+10)
+    for e in range(0,len(personas)):
+        if personas[e][0] > i and personas[e][0] <= i+10:
+            edadpersonas.append(personas[e])
+    personas_por_edad[tuplaedad]=edadpersonas
+    i=i+10
+
+print(personas_por_edad)
+    
+
+
+
+
+
--- ej diccionarios.py
+++ ej diccionarios.py
@@ -3,8 +3,20 @@ Abecedario = {}
 largo=len(letras)
 for letra in range(largo):
     Abecedario[letra]=letras[letra]
-print(Abecedario)
+#print(Abecedario)
 for i in list(Abecedario):
     if i%3==0 and i>0:
         Abecedario.pop(i)
-print(Abecedario)
+#print(Abecedario)
+
+lista=[]
+for letra in range(largo):
+    lista.append(letra)
+print(lista)
+
+llista=len(lista)
+for i in range(0,llista):
+    if i%3==0:
+        lista.pop(i)
+        i=i-1
+print(lista)
