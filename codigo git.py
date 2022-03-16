bae74534033a1a86829f3191bfb927a074848fce
Juan Esteban <jegarnham@uc.cl>
2022-03-15 23:36:07 -0300

    ultimo avance de hoy

--- codigo git.py
+++ codigo git.py
@@ -1,24 +1,29 @@
 class Repositorio:
     
     def __init__(self, archivos=[]):
-        self.archivos_staging = archivos
-        self.archivos_espera = []
         self.archivos_remotos = []
+        self.archivos_locales = archivos
+        self.archivos_staging = []
         #puedes agregar más atributos si lo estimas necesario ;)
         
     def git_add(self, archivos):
-        
-
-        pass
+        for archivo in archivos:
+            self.archivos_staging.append(archivo)
+        #falta chequeo de que no se repitan
+    
         
     def git_commit(self, comentario):
+        for doc in self.archivos_staging:
+            self.archivos_locales.append(doc)
         print(comentario)
-        #debes completar aquí
-        pass
+        
     
     def git_push(self):
-        #debes completar aquí
-        pass
+        largo=len(self.archivos_locales)
+        for x in range (0,largo-1):
+            y=self.archivos_locales.pop(0)
+            self.archivos_remotos.append(y)
+
 
 
 if __name__ == "__main__":
