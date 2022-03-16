448ad6edbef4cdde15ed09c4bc0e794eb1830a99
Juan Esteban <jegarnham@uc.cl>
2022-03-16 11:39:37 -0300

    codigo git sin validacion de repetidos

--- codigo git.py
+++ codigo git.py
@@ -4,25 +4,29 @@ class Repositorio:
         self.archivos_remotos = []
         self.archivos_locales = archivos
         self.archivos_staging = []
-        #puedes agregar más atributos si lo estimas necesario ;)
         
-    def git_add(self, archivos):
-        for archivo in archivos:
+    def git_add(self, *args):
+        for archivo in args:
             self.archivos_staging.append(archivo)
+        print("archivos en staging: " + str(self.archivos_staging))
         #falta chequeo de que no se repitan
     
         
     def git_commit(self, comentario):
         for doc in self.archivos_staging:
+            doc=self.archivos_staging.pop(0)
             self.archivos_locales.append(doc)
         print(comentario)
+        print("archivos en local: " + str(self.archivos_locales))
         
     
     def git_push(self):
         largo=len(self.archivos_locales)
-        for x in range (0,largo-1):
-            y=self.archivos_locales.pop(0)
-            self.archivos_remotos.append(y)
+        largoremoto=len(self.archivos_remotos)
+        for x in range (0,largo):
+            x=self.archivos_locales.pop(0)
+            self.archivos_remotos.append(x)
+        print("archivos en remoto: " + str(self.archivos_remotos))
 
 
 
